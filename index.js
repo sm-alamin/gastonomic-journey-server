a489c54eacafc745e9e4e97dad46dd974ed052c3
sm-alamin <sma.fkml@gmail.com>
2023-05-02 11:11:36 +0600

    chef info data is added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5000;
+
+const chef_info = require('./data/chef_info.json');
 
 app.get('/', (req, res) => {
   res.send('the server of chef recipe hunter app is running...')
 })
-
+app.get('/chef-info', (req, res) => {
+    res.send(chef_info);
+})
 app.listen(port, () => {
   console.log(`chef recipe hunter is running on port ${port}`)
 })
