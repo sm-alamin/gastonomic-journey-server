1e20b4487ff9e04c02b618a8b64b079dc2ad8444
sm-alamin <sma.fkml@gmail.com>
2023-05-02 12:13:16 +0600

    cors installation is done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000;
+
+app.use(cors())
 
 const chef_info = require('./data/chef_info.json');
 
